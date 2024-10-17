2950024dd1a6513aaea42b348a70bc2bcddf6c30
niphoka <86031175+niphoka@users.noreply.github.com>
2024-10-17 13:48:03 -0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,17 @@
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is an exercise for variables \n");
+        int myNum = 3;
+        char myChar = 'B';
+        String myString = "What goes around comes around";
+        boolean myBool = false;
+        final int myFin = 500;
+        System.out.println("This will print an integer: " + myNum);
+        System.out.println("This will print a character: " + myChar);
+        System.out.println("This will print a string: " + myString);
+        System.out.println("This will print a boolean: " + myBool);
+        System.out.println("This will print a final / constant: " + myFin);
+    }
+}
