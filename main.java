703ee961b7892249e3d8e3bb57024b19cfe9c255
niphoka <86031175+niphoka@users.noreply.github.com>
2024-10-18 17:36:12 -0600

    Update main.java

--- main.java
+++ main.java
@@ -15,3 +15,36 @@ public class Main
         System.out.println("This will print a final / constant: " + myFin);
     }
 }
+
+
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is a practice for casting\n");
+
+        System.out.println("Wide casting (from small to large)");
+        int myInt = 30;
+        double newDoub = myInt;
+        System.out.println(newDoub);
+        System.out.println();
+        
+        System.out.println("Narrow casting (from large to small)");
+
+        double myDoub = 12.99;
+        int newInt = (int) myDoub;
+        System.out.println(newInt);
+        System.out.println();
+        
+        System.out.println("Real life example");
+    
+        int studentPoints = 70;
+        int totalPoints = 80;
+        System.out.println("If a student gets 70 out of 80 points on a exam, the final grade is: " + (float) studentPoints / totalPoints * 100);
+        /*We need to convert studentPoints or totalPoints (one or the other) variables from integer to double so that result is accurate, otherwise, it will always end up in zero (0), see below:
+        + studentPoints / totalPoints * 100); THIS WILL RESULT IN 0, A FAILURE IN THE CODE
+        */
+    }
+}
