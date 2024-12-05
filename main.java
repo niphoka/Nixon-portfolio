77072f65dfa50b2651d10bacdb4e2025d30460d3
niphoka <86031175+niphoka@users.noreply.github.com>
2024-12-05 15:17:31 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1107,9 +1107,38 @@ public class Main
 class Second {
   public static void main(String[] args) 
   {
-    System.out.println("***Multiple classes***)
+    System.out.println("***Multiple classes***\n")
     Main myCar = new Main();     // Create a myCar object
     myCar.fullThrottle();      // Call the fullThrottle() method
     myCar.speed(200);          // Call the speed() method
   }
 }
+
+
+
+
+
+public class Main //Class
+{
+    int x;//Attribute
+    
+    public Main()//Constructor
+    {
+        x = 5;//Initial value
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.println("***Constructors***\n");
+        Main myObj = new Main();//Call constructor
+        System.out.println(myObj.x);//Print value of 5
+    }
+}
+
+/*Note that the constructor name must match the class name, and it cannot have a return type (like void).
+
+Also note that the constructor is called when the object is created.
+
+All classes have constructors by default: if you do not create a class constructor yourself, Java creates one for you. 
+However, then you are not able to set initial values for object attributes.*/
+
