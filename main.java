8fd6b30c10b1ca1a42a7f7fd2280183365acd4d4
niphoka <86031175+niphoka@users.noreply.github.com>
2024-12-10 16:04:16 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1253,3 +1253,49 @@ class Second
         myObj.study();
     }
 }
+
+
+
+
+
+
+public class Person/*This must done/created/written on a separate file due to message:
+
+error: class Person is public, should be declared in a file named Person.java
+public class Person*/
+
+{
+    private String name;//Variable
+    
+    public String getName()//Variable with caps
+    {
+        return name;
+    }
+    
+    public void setName(String newName)//Variable with caps and method with parameter
+    {
+        this.name = newName;
+        //The "this" keyword is used to refer to the current object/variable
+    }
+}
+
+/*The get method returns the variable value, and the set method sets the value.
+
+Syntax for both is that they start with either get or set, followed by the name of the variable, with the first letter in upper case*/
+
+public class Main
+
+{
+    public static void main(String[] args)
+    {
+        System.out.println("***Encapsulation***/n");
+        Person myObj = new Person();
+        myObj.setName("John");//This is done differently from usual when calling method
+        System.out.println(myObj.getName);//Pay attention to this as well
+    }
+}
+/*Why Encapsulation?
+Better control of class attributes and methods
+Class attributes can be made read-only (if you only use the get method), or write-only (if you only use the set method)
+Flexible: the programmer can change one part of the code without affecting other parts
+Increased security of data*/
