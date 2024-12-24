6cbf1bd038556987325453171be0e43a1496fae8
niphoka <86031175+niphoka@users.noreply.github.com>
2024-12-24 12:40:50 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1661,3 +1661,38 @@ public class Main
         }    
     }
 }
+
+
+
+
+
+
+
+enum Level
+{
+    LOW,
+    MEDIUM,
+    HIGH
+}
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("***Loop through an Enum***\n");
+        for (Level myLevel : Level.values())
+        {
+            System.out.println(myLevel);
+        }
+        
+    }
+}
+
+/* Difference between Enums and Classes
+An enum can, just like a class, have attributes and methods. The only difference is that enum constants are public, static and final (unchangeable - cannot be overridden).
+
+An enum cannot be used to create objects, and it cannot extend other classes (but it can implement interfaces).
+
+Why And When To Use Enums?
+Use enums when you have values that you know aren't going to change, like month days, days, colors, deck of cards, etc.*/
