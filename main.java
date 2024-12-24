56954f81077b4462b3045bfcc49cbc58fbbd53f1
niphoka <86031175+niphoka@users.noreply.github.com>
2024-12-23 19:24:21 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1542,3 +1542,65 @@ class Main
 
 To achieve security - hide certain details and only show the important details of an object
 Note: Abstraction can also be achieved with Interfaces */
+
+
+
+
+
+
+interface Animal
+{
+    public void animalSound();//Methods do not have body 
+    public void sleep();
+}
+
+class Pig implements Animal//Pay attention to implements instead of extends
+{
+    public void animalSound()//Body is implemented here
+    {
+        System.out.println("The pig goes: oink oink");
+    }
+    
+    public void sleep()
+    {
+        System.out.println("Zzzzzzzz");
+    }
+}
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("***Interfaces - Abstraction***\n");
+        Pig myPig = new Pig();
+        myPig.animalSound();
+        myPig.sleep();
+    }
+}
+
+/* Notes on Interfaces:
+
+Like abstract classes, interfaces cannot be used to create objects (in the example above, it is not possible to create an "Animal" 
+object in the Main Class)
+
+Interface methods do not have a body - the body is provided by the "implement" class
+
+On implementation of an interface, you must override all of its methods
+
+Interface methods are by default abstract and public
+
+Interface attributes are by default public, static and final
+
+An interface cannot contain a constructor (as it cannot be used to create objects)
+
+Why And When To Use Interfaces?
+1) To achieve security - hide certain details and only show the important details of an object (interface).
+
+2) Java does not support "multiple inheritance" (a class can only inherit from one superclass). However, it can be achieved with 
+interfaces, because the class can implement multiple interfaces. 
+
+Note: To implement multiple interfaces, separate them with a comma.
+
+class DemoClass implements FirstInterface, SecondInterface {...}
+
+*/
