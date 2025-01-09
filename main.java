db84fba3d48f3fd5c20923f3cf889d43e28c5254
niphoka <86031175+niphoka@users.noreply.github.com>
2025-01-09 11:37:20 -0600

    Update main.java

--- main.java
+++ main.java
@@ -2199,5 +2199,62 @@ Exception in thread "main" java.lang.ArithmeticException: Age less than 18
 
 ===============================================================================================================================================
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("***RegEx***\n");
+        /* Flags in the compile() method change how the search is performed. Here are a few of them:
+
+Pattern.CASE_INSENSITIVE - The case of letters will be ignored when performing a search.
+Pattern.LITERAL - Special characters in the pattern will not have any special meaning and will be treated as ordinary characters when performing a search.
+Pattern.UNICODE_CASE - Use it together with the CASE_INSENSITIVE flag to also ignore the case of letters outside of the English alphabet */
+
+        Pattern pat = Pattern.compile("Maya|Luna|Bonete", Pattern.CASE_INSENSITIVE);
+            // Find a match for any one of the patterns separated by "|"
+        Matcher mat = pat.matcher("My child is called Maya mom!");
+        
+        /*In this example, The words "Maya, or Luna, or Bonete" are being searched for in a sentence.
+
+First, the pattern is created using the Pattern.compile() method. The first parameter indicates which pattern is being searched for and the second parameter has a flag to indicates that the search should be case-insensitive. The second parameter is optional.
+
+The matcher() method is used to search for the pattern in a string. It returns a Matcher object which contains information about the search that was performed.
+
+The find() method returns true if the pattern was found in the string and false if it was not found.*/
+
+        
+        // Pattern pat = Pattern.compile("[0-9]");
+            // Find one character from the range 0 to 9
+        // Matcher mat = pat.matcher("7 is my favorite number");
+        
+        // Pattern pat = Pattern.compile("[aeiou]", Pattern.CASE_INSENSITIVE); 
+            // Find one character from the options between the brackets
+        // Matcher mat = pat.matcher("Hello world from Java"); 
+        
+        // Pattern pat = Pattern.compile("m+", Pattern.CASE_INSENSITIVE); 
+            // Matches any string that contains at least one m
+        // Matcher mat = pat.matcher("My child is called Maya mom!"); 
+        
+        // Pattern pat = Pattern.compile("m{1,3}", Pattern.CASE_INSENSITIVE);
+            // Matches any string that contains a sequence of X to Y n's
+        // Matcher mat = pat.matcher("My child is called Maya mom!");        
+        
+        boolean matchFound = mat.find();
+        if (matchFound)
+        {
+            System.out.println("Match found");
+        }
+        else
+        {
+            System.out.println("No match found");
+        }
+    }
+}
+
+===============================================================================================================================================
+
+
+	
