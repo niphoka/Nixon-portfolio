6067cf75e7a4d98556728bc1bf0ba367b9b09a8b
niphoka <86031175+niphoka@users.noreply.github.com>
2025-01-13 18:04:16 -0600

    Update main.java

--- main.java
+++ main.java
@@ -2256,5 +2256,89 @@ The find() method returns true if the pattern was found in the string and false 
 
 ===============================================================================================================================================
 
+    //Threads allows a program to operate more efficiently by doing multiple things at the same time.
+
+    // Threads can be used to perform complicated tasks in the background without interrupting the main program.
+
+public class Main extends Thread
+{
+    public static void main(String[] args)
+    {
+        System.out.println("***Threads****\n");
+        Main thread = new Main();
+        thread.start();
+        System.out.println("This code is running outside of the thread");
+    }
+    
+    // A thread can be created by extending the Thread class and overriding its run() method
+    
+    public void run()
+    {
+        System.out.println("This code is running in a thread");
+    }
+}
+
+===============================================================================================================================================
+
+public class Main implements Runnable
+{
+    public static void main(String[] args)
+    {
+        System.out.println("***Threads with Runnable interface***\n");
+        Main obj = new Main();
+        Thread thread = new Thread(obj);
+        thread.start();
+        System.out.println("This code us running outside of the thread");
+    }
+    
+    public void run()
+    {
+        System.out.println("This code is running in a thread");
+    }
+}
+
+/* Differences between "extending" and "implementing" Threads
+
+The major difference is that when a class extends the Thread class, you cannot extend any other class, 
+but by implementing the Runnable interface, it is possible to extend from another class as well, like: 
+class MyClass extends OtherClass implements Runnable. */
+
+===============================================================================================================================================
+
+/*Concurrency Problems
+Because threads run at the same time as other parts of the program, there is no way to know in which order the code will run. 
+When the threads and main program are reading and writing the same variables, the values are unpredictable. 
+The problems that result from this are called concurrency problems.*/
+
+public class Main extends Thread
+{
+    public static int amount = 0;
+    public static void main(String[] args)
+    {
+        Main thread = new Main();
+        thread.start();
+        
+        /*To avoid concurrency problems, it is best to share as few attributes between threads as possible. 
+	If attributes need to be shared, one possible solution is to use the isAlive() method of the thread to check whether 
+ 	the thread has finished running before using any attributes that the thread can change.*/
+        
+        while (thread.isAlive())//Without this, values are unpredictable (unordered)
+        {
+            System.out.println("Waiting...");
+        }
+        
+        System.out.println("Main: " + amount);
+        amount++;
+        System.out.println("Main: " + amount);
+    }
+    
+    public void run()
+    {
+        amount++;
+    }
+}	
+
+===============================================================================================================================================
+
 
 	
