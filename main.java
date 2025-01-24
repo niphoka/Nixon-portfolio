884ae778fa8822e9c53b2b8f274e9f8d2da4424e
niphoka <86031175+niphoka@users.noreply.github.com>
2025-01-23 18:03:13 -0600

    Update main.java

--- main.java
+++ main.java
@@ -2724,5 +2724,28 @@ public class Main
 
 ===============================================================================================================================================
 
+import java.io.File;
 
-	
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("***File handling - Get file information***\n");
+        File file = new File("test.txt");
+        if (file.exists())
+        {
+            System.out.println("File name: " + file.getName());
+            System.out.println("Absolute path: " + file.getAbsolutePath());
+            System.out.println("Writeable: " + file.canWrite());
+            System.out.println("Readable: " + file.canRead());
+            System.out.println("File size in bytes: " + file.length());
+        }
+        else
+        {
+            System.out.println("The file does not exist");
+        }
+    }
+}
+
+===============================================================================================================================================
+
