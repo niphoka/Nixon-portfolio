273ab8085cce56e77e64c85fb1dd8ea5834a2e79
niphoka <86031175+niphoka@users.noreply.github.com>
2025-01-30 17:43:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -923,4 +923,26 @@ print()
 
 ==================================================================================================================================================
 
+print("***Iterator - Manual iteration***\n")
+
+'''Create an iterator that returns numbers, starting with 1, and each sequence will increase by one (returning 1,2,3,4,5 etc.)'''
+
+class Numbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+aClass = Numbers()
+iterable = iter(aClass)
+print(next(iterable))# This is a manual approach of doing the iteration. one by one
+print(next(iterable))
+print(next(iterable))
+
+==================================================================================================================================================
+
 
