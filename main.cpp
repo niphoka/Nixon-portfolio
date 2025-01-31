21cc36e8b12454ff9bffd7e404cdede90e93bdef
niphoka <86031175+niphoka@users.noreply.github.com>
2025-01-31 17:54:19 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2205,3 +2205,39 @@ int main()
 
 ==================================================================================================================================================
 
+#include <iostream>
+#include <stack> 
+
+using namespace std;
+
+/* A stack stores multiple elements in a specific order, called LIFO.
+
+LIFO stands for Last in, First Out. To vizualise LIFO, think of a pile of pancakes, where pancakes are both added and removed from the top. So when removing a pancake, it will always be the last one you added. This way of organizing elements is called LIFO in computer science and programming.
+
+Unlike vectors, elements in the stack are not accessed by index numbers. Since elements are added and removed from the top, you can only access the element at the top of the stack */
+
+int main()
+{
+    cout << "***Stacks***\n\n";
+    stack<string> cars; // You cannot add elements to the stack at the time of declaration, like you can with vectors
+
+    cars.push("Lamborghini");
+    cars.push("Alpha Romeo");
+    cars.push("Ferrari");// Last is first/item on top
+    
+    cout << cars.top() << endl;// Shows top element
+
+    cars.top() = "Rolls Royce";// Changes value of top element
+    cout << cars.top() << endl;
+
+    cars.pop();// Removes last added element
+    cout << cars.top() << endl;
+
+    cout << cars.size() << endl;
+
+    cout << cars.empty() << endl;// Returns 1 (true) if the stack is empty and 0 (false) otherwise
+}
+
+==================================================================================================================================================
+
+  
