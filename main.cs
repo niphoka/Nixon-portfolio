b4fa692daff98dbf0d5f4305d4bf870923b5cc62
niphoka <86031175+niphoka@users.noreply.github.com>
2025-02-03 14:20:50 -0600

    Update main.cs

--- main.cs
+++ main.cs
@@ -1583,5 +1583,55 @@ Note: Abstraction can also be achieved with Interfaces */
 
 ==================================================================================================================================================
 
- 
+using System;
+
+/* An interface is a completely "abstract class", which can only contain abstract methods and properties (with empty bodies) */
+
+interface Animal
+{
+    void sound();// interface method (does not have a body)
+}
+
+class Pig : Animal// Pig "implements" the Animal interface
+{
+    public void sound()
+    {
+        Console.WriteLine("The pig goes: oink oink oink");// The body of sound() is provided here
+    }
+}
+
+/* It is considered good practice to start with the letter "I" at the beginning of an interface, as it makes it easier for yourself and others to remember that it is an interface and not a class.
+
+By default, members of an interface are abstract and public.
+
+Note: Interfaces can contain properties and methods, but not fields. */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("***Interface***\n");
+        Pig pig = new Pig();// Create a Pig object
+        pig.sound();
+    }
+}
+
+/* Notes on Interfaces:
+
+Like abstract classes, interfaces cannot be used to create objects (in the example above, it is not possible to create an "IAnimal" object in the Program class)
+Interface methods do not have a body - the body is provided by the "implement" class
+On implementation of an interface, you must override all of its methods
+Interfaces can contain properties and methods, but not fields/variables
+Interface members are by default abstract and public
+An interface cannot contain a constructor (as it cannot be used to create objects)
+
+Why And When To Use Interfaces?
+
+1) To achieve security - hide certain details and only show the important details of an object (interface).
+
+2) C# does not support "multiple inheritance" (a class can only inherit from one base class). However, it can be achieved with interfaces, because the class can implement multiple interfaces. Note: To implement multiple interfaces, separate them with a comma */
+    
+==================================================================================================================================================
+
+
     
